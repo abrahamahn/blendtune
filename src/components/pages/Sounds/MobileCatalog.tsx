335856b3f941e8296a867fec02f45fcd7f71d0da
Abe Ahn <abemsq@gmail.com>
2023-12-14 17:41:37 +0900

    commit

--- src/components/pages/Sounds/MobileCatalog.tsx
+++ src/components/pages/Sounds/MobileCatalog.tsx
@@ -119,19 +119,19 @@ const MobileCatalog: React.FC<MobileCatalogProps> = ({
               </div>
             </div>
             <div className='absolute flex right-0 lg:right-6 items-center group-hover:shadow-3xl group-hover:shadow-neutral-100 dark:group-hover:shadow-neutral-900 '>
-              <div className='text-neutral-500 group-hover:bg-neutral-100 dark:text-neutral-300 p-3 dark:group-hover:bg-neutral-900'>
+              <div className='text-neutral-500 group-hover:bg-neutral-100 dark:text-neutral-300 p-2.5 sm:p-3 dark:group-hover:bg-neutral-900'>
                 <FontAwesomeIcon
                   icon={faHeart}
                   className='invisible group-hover:visible '
                 />
               </div>
-              <div className='text-neutral-500 group-hover:bg-neutral-100 dark:text-neutral-300 p-3 dark:group-hover:bg-neutral-900'>
+              <div className='text-neutral-500 group-hover:bg-neutral-100 dark:text-neutral-300 p-2.5 sm:p-3 dark:group-hover:bg-neutral-900'>
                 <FontAwesomeIcon
                   icon={faPlus}
                   className='invisible group-hover:visible'
                 />
               </div>
-              <div className='dark:text-white text-neutral-500 flex justify-center p-3 mr-1'>
+              <div className='dark:text-white sm:p-3 text-neutral-500 flex justify-center p-2.5 mr-1'>
                 <FontAwesomeIcon icon={faEllipsisVertical} />
               </div>
             </div>
